e721aecce7837c6244000210eea35518957dae97
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-06-27 21:51:39 +0100

    Add all-auth apps/templates and configure

--- gym_zen/settings.py
+++ gym_zen/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_sass',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 MIDDLEWARE = [
