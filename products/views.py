8aa4fd00d8c6a93958ea43a35a75767cfdf4a815
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-09-18 20:42:08 +0100

    Add col sizes for media on product cards

--- products/views.py
+++ products/views.py
@@ -26,6 +26,7 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
