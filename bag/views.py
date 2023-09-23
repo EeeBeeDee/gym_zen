bd7b2ad5d9ae571db34d1fb0888152e7ccad54a0
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-09-23 23:16:46 +0100

    Add bag session functionality

--- bag/views.py
+++ bag/views.py
@@ -10,6 +10,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
+    print(quantity)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {} )
 
@@ -19,4 +20,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
