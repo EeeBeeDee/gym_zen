e73516a4c0bc60d44ecd8c5ece72bb0898c9d1c6
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-09-24 14:51:26 +0100

    Add basic bag HTML and responsivness

--- bag/views.py
+++ bag/views.py
@@ -10,15 +10,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
-    print(quantity)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {} )
-
+         
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+    print(bag)
     return redirect(redirect_url)
