82a6f1e6879643ebb6b2c80e6b150f7001405a5b
EeeBeeDee <eoghanbehandoyle@gmail.com>
2024-01-07 20:17:24 +0000

    Create basic order model for form testing

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    # inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_number', 'date', 'grand_total')
+
+    fields = ('order_number', 'full_name', 'email', 'phone_number', 'eircode', 'city', 'street_address', 'county', 'country', 'date', 'grand_total')
+
+    list_display = ('order_number', 'date', 'full_name', 'grand_total')
+
+    ordering = ('-date', )
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    order_number = models.UUIDField(default=uuid.uuid4, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    eircode = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=40, null=False, blank=False)
+    street_address = models.CharField(max_length=80, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def __str__(self):
+        return self.order_number
